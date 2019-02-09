7094c1939944f3204092e1249205a0d95896e193
Yaroslav <git@unimarijo.com>
2019-02-10 01:32:05 +0400

    WIP: Теперь ошибки отправляются в Telegram
    * При включенном DEVELOPER_MODE
    
    * Пока только в методе, который отвечает за ответ на сообщения из VK

--- app/telegram/message_handler.py
+++ app/telegram/message_handler.py
@@ -20,20 +20,24 @@ def message_handler(client, message):
             assert message.reply_to_message
         except AssertionError:
             return
-
         tg_message_id = "{0}_{1}".format(message.reply_to_message.chat.id, message.reply_to_message.message_id)
         vk_message_id = asyncio.get_event_loop().run_until_complete(
             redis.execute("GET", "message:telegram:{0}".format(tg_message_id)))['details']
         if not vk_message_id:
-            logging.debug("Получен reply на сообщение, но к нему не привязан никакой ID сообщения из VK.")
+            logging.debug("Сообщение, на которое ответил пользователь, не содержит ID сообщения из VK.")
+            if config.DEVELOPER_MODE:
+                message.reply("ℹ️ К сообщению, на которое вы ответили, не привязано ID сообщения из VK.")
             return
 
-        logging.debug("Получен reply на сообщение из VK с ID: {0}".format(vk_message_id))
         user = asyncio.get_event_loop().run_until_complete(hgetall("user:{0}".format(message.from_user.id)))
         data = {"peer_id": vk_message_id.split("_")[0], "random_id": randbits(32), "message": message.text,
                 "access_token": user['VK_TOKEN'], "v": 5.92}
         response = requests.post("https://api.vk.com/method/messages.send",
                                  data=sign_data(data, "messages.send", user['VK_SECRET'])).json()
+        logging.debug("Была совершена попытка отправки сообщения, VK вернул: {0}".format(response))
+        if config.DEVELOPER_MODE:
+            message.reply("ℹ️ Была совершена попытка отправки сообщения, VK вернул:\n\n`{0}`.".format(response))
+
         return response
     except Exception as e:
         logging.error("Произошла ошибка при попытке выполнения метода.", exc_info=True)
