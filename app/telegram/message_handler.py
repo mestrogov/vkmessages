2954792216d7569567348e07490d7692397f0477
Yaroslav <git@unimarijo.com>
2019-02-11 22:53:52 +0400

    Косметические изменения

--- app/telegram/message_handler.py
+++ app/telegram/message_handler.py
@@ -26,7 +26,8 @@ def message_handler(client, message):
         if not vk_message_id:
             logging.debug("Сообщение, на которое ответил пользователь, не содержит ID сообщения из VK.")
             if config.DEVELOPER_MODE:
-                message.reply("ℹ️ К сообщению, на которое вы ответили, не привязано ID сообщения из VK.")
+                message.reply("ℹ️ К сообщению, на которое вы ответили, не привязано ID сообщения из VK.",
+                              disable_notification=True)
             return
 
         user = asyncio.get_event_loop().run_until_complete(hgetall("user:{0}".format(message.from_user.id)))
@@ -34,9 +35,10 @@ def message_handler(client, message):
                 "access_token": user['VK_TOKEN'], "v": 5.92}
         response = requests.post("https://api.vk.com/method/messages.send",
                                  data=sign_data(data, "messages.send", user['VK_SECRET'])).json()
-        logging.debug("Была совершена попытка отправки сообщения, VK вернул: {0}".format(response))
+        logging.debug("Была совершена попытка отправки сообщения, VK вернул ответ: {0}".format(response))
         if config.DEVELOPER_MODE:
-            message.reply("ℹ️ Была совершена попытка отправки сообщения, VK вернул:\n\n`{0}`.".format(response))
+            message.reply("ℹ️ Была совершена попытка отправки сообщения, VK вернул ответ:\n\n`{0}`.".format(response),
+                          disable_notification=True)
 
         return response
     except Exception as e:
