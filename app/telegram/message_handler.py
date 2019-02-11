ab11233b59c151c856ae54655d4064b71749f4a2
Yaroslav <git@unimarijo.com>
2019-02-12 00:01:39 +0400

    WIP: Отправка стикеров из Telegram в VK

--- app/telegram/message_handler.py
+++ app/telegram/message_handler.py
@@ -5,6 +5,8 @@ from app.utils.redis_hgetall import redis_hgetall as hgetall
 from app.remote.redis import Redis as redis
 from app import config as config
 from app.vk.utils.sign_data import sign_data
+from PIL import Image
+from tempfile import NamedTemporaryFile
 from secrets import randbits
 import requests
 import logging
@@ -31,6 +33,45 @@ def message_handler(client, message):
             return
 
         user = asyncio.get_event_loop().run_until_complete(hgetall("user:{0}".format(message.from_user.id)))
+
+        try:
+            assert message.sticker.file_id
+            with NamedTemporaryFile(suffix=".webp") as sticker_webp_file:
+                logging.debug("Стикер скачивается во временный файл.")
+                client.download_media(message.sticker.file_id, sticker_webp_file.name)
+                sticker_webp = Image.open(sticker_webp_file.name)
+                with NamedTemporaryFile(suffix=".png") as sticker_png_file:
+                    sticker_webp.save(sticker_png_file.name, format="PNG")
+                    logging.debug(
+                        "Стикер переконвертирован в формат PNG, сохранен в файл: {0}".format(sticker_png_file.name))
+
+                    data = {"type": "graffiti", "access_token": user['VK_TOKEN'], "v": 5.92}
+                    upload_server = requests.post("https://api.vk.com/method/docs.getUploadServer",
+                                                  data=sign_data(data, "docs.getUploadServer", user['VK_SECRET'])).json()
+                    logging.debug("URL сервера, на который будет происходить загрузка файла: {0}".format(
+                        upload_server['response']['upload_url']))
+
+                    files = {'file': open(sticker_png_file.name, 'rb')}
+                    uploaded_file = requests.post(upload_server['response']['upload_url'], files=files).json()
+                    logging.debug("Загружен стикер на сервера VK, результат: {0}".format(uploaded_file))
+
+                    data = {"title": "sticker.png", "tags": "стикер", "file": uploaded_file['file'],
+                            "access_token": user['VK_TOKEN'], "v": 5.92}
+                    saved_document = requests.post("https://api.vk.com/method/docs.save",
+                                                   data=sign_data(data, "docs.save", user['VK_SECRET'])).json()
+                    logging.debug("Сохранен стикер как документ, результат: {0}".format(saved_document))
+
+                    data = {"peer_id": vk_message_id.split("_")[0], "random_id": randbits(32),
+                            "attachment": "doc{0}_{1}".format(saved_document['response']['graffiti']['owner_id'],
+                                                              saved_document['response']['graffiti']['id']),
+                            "access_token": user['VK_TOKEN'],
+                            "v": 5.92}
+                    graffiti_message = requests.post("https://api.vk.com/method/messages.send",
+                                                     data=sign_data(data, "messages.send", user['VK_SECRET'])).json()
+                    logging.debug("Сообщение со стикером отправлено, результат: {0}".format(graffiti_message))
+        except AssertionError:
+            logging.debug("Похоже, что это сообщение не является стикером..")
+
         data = {"peer_id": vk_message_id.split("_")[0], "random_id": randbits(32), "message": message.text,
                 "access_token": user['VK_TOKEN'], "v": 5.92}
         response = requests.post("https://api.vk.com/method/messages.send",
